b38b97750ed96c66af93aa936aac566434563986
Alexander Legotin <mrproz@yandex.ru>
2018-06-02 22:34:51 +0300

    Both projects moved to containers

--- RequestSenderSimple/Program.cs
+++ RequestSenderSimple/Program.cs
@@ -10,16 +10,16 @@ namespace RequestSenderSimple
 
         public static void Main(string[] args)
         {
-            var uri = "http://192.168.1.25:6075/";
-            var name = "Unnamed container";
+            var uri = "http://httplistenersimple:777/";
+            var name = String.Format("{0:X}", Guid.NewGuid().GetHashCode()); ;
             var nameFromEnvVar = Environment.GetEnvironmentVariable("CONNAME");
             if (!string.IsNullOrEmpty(nameFromEnvVar)) name = nameFromEnvVar;
-            Console.WriteLine($"Starting...{uri}, my name is {name}");
-            while (failCount < 10)
+            Console.WriteLine($"Starting...Sending to {uri}, my name is {name}");
+            while (failCount < 20)
                 try
                 {
                     var random = new Random(DateTime.Now.Millisecond);
-                    var howLongToSleep = random.Next(10, 500);
+                    var howLongToSleep = random.Next(500, 5000);
                     Thread.Sleep(howLongToSleep);
                     var response = HttpHelper.PostSync(uri, new SimpleRequest { Name = name, Slept = howLongToSleep });
                     Console.WriteLine(response);
